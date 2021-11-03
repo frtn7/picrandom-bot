da629c764ac1331c9edf044d94f415e94d2e4388
Dmitry Ivanoff <gausspoint34@gmail.com>
2021-11-03 22:28:04 +0300

    test: bot image grab

--- routes/botman.php
+++ routes/botman.php
@@ -2,14 +2,9 @@
 use App\Http\Controllers\BotManController;
 use Buchin\GoogleImageGrabber\GoogleImageGrabber;
 use Illuminate\Support\Facades\Storage;
+use BotMan\BotMan\Messages\Attachments\Image;
 
 $botman = resolve('botman');
-
-//$botman->hears('Hi', function ($bot) {
-//    $bot->reply('Hello!');
-//});
-
-//$botman->hears('Start conversation', BotManController::class.'@startConversation');
 
 $botman->hears('/pic {keyword}', function ($bot, $keyword) {
     $images = GoogleImageGrabber::grab($keyword);
@@ -18,24 +13,7 @@ $botman->hears('/pic {keyword}', function ($bot, $keyword) {
     $pic_content = file_get_contents($url);
     $name = substr($url, strrpos($url, '/') + 1);
     Storage::put('public/images/' . $name, $pic_content);
+    $attachment = new Image($url);
     $bot->reply('Your pic is here:');
-    $bot->reply(Storage::get($name));
+    $bot->reply($attachment);
 });
-
-//$url = $images[$rand_index]['url'];
-//$contents = file_get_contents($url);
-//$name = substr($url, strrpos($url, '/') + 1);
-//Storage::put($name, $contents);
-
-//$keyword = 'dog';
-//$images = GoogleImageGrabber::grab($keyword);
-//$rand_index = rand(0, count($images));
-//$url = $images[$rand_index]['url'];
-//$pic_content = file_get_contents($url);
-//$name = substr($url, strrpos($url, '/') + 1);
-//Storage::put('public/images/' . $name, $pic_content);
-
-//echo '<pre>';
-//print_r($images[$rand_index]['url']);
-//print_r($pic_content);
-//echo '</pre>';
