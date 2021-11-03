82961361204414a8d7fe06be79548de362b9b643
Dmitry Ivanoff <gausspoint34@gmail.com>
2021-11-03 23:31:07 +0300

    feat: multiple and single pic commands

--- routes/botman.php
+++ routes/botman.php
@@ -7,10 +7,24 @@ use BotMan\BotMan\Messages\Outgoing\OutgoingMessage;
 
 $botman = resolve('botman');
 
-$botman->hears('/pic {keyword}', function ($bot, $keyword) {
-    $bot->reply('Your pics are here:');
+$botman->hears('/singlepic {keyword}', function ($bot, $keyword) {
+    $bot->reply('Your picture is here:');
+    $images = GoogleImageGrabber::grab($keyword);
+    $rand_index = rand(0, count($images));
+    $url = $images[$rand_index]['url'];
+    $pic_content = file_get_contents($url);
+    $name = substr($url, strrpos($url, '/') + 1);
+    Storage::put('public/images/' . $name, $pic_content);
+    $attachment = new Image($url);
+    $message = OutgoingMessage::create()
+        ->withAttachment($attachment);
+    $bot->reply($message);
+});
+
+$botman->hears('/multipic {number} {keyword}', function ($bot, $number, $keyword) {
+    $bot->reply('Your pictures are here:');
     $i = 0;
-    while ($i++ < 6) {
+    while ($i++ < $number) {
         $images = GoogleImageGrabber::grab($keyword);
         $rand_index = rand(0, count($images));
         $url = $images[$rand_index]['url'];
