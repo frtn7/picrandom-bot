d1402f6aadc81550e9608a349bb652c440533903
Dmitry Ivanoff <gausspoint34@gmail.com>
2021-11-03 21:15:35 +0300

    test: bot image grab

--- routes/botman.php
+++ routes/botman.php
@@ -1,9 +1,29 @@
 <?php
 use App\Http\Controllers\BotManController;
+use Buchin\GoogleImageGrabber\GoogleImageGrabber;
 
 $botman = resolve('botman');
 
-$botman->hears('Hi', function ($bot) {
-    $bot->reply('Hello!');
+//$botman->hears('Hi', function ($bot) {
+//    $bot->reply('Hello!');
+//});
+
+//$botman->hears('Start conversation', BotManController::class.'@startConversation');
+
+$botman->hears('/pic {keyword}', function ($bot, $keyword) {
+    $images = GoogleImageGrabber::grab($keyword);
+    $rand_index = rand(0, count($images));
+    $bot->reply('Your pic is here:');
+    $bot->reply($images[$rand_index]['url']);
 });
-$botman->hears('Start conversation', BotManController::class.'@startConversation');
+
+//$keyword = 'dog';
+//
+//$images = GoogleImageGrabber::grab($keyword);
+//
+//$rand_index = rand(0, count($images));
+//
+//echo '<pre>';
+//print_r($images[$rand_index]['url']);
+//print_r($images);
+//echo '</pre>';
