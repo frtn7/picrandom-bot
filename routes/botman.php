8b1d2ccdaa5bb1a5d9e1a7bd5ded479ed221a690
Dmitry Ivanoff <gausspoint34@gmail.com>
2021-11-03 23:15:19 +0300

    fix: multiple

--- routes/botman.php
+++ routes/botman.php
@@ -8,15 +8,18 @@ use BotMan\BotMan\Messages\Outgoing\OutgoingMessage;
 $botman = resolve('botman');
 
 $botman->hears('/pic {keyword}', function ($bot, $keyword) {
-    $images = GoogleImageGrabber::grab($keyword);
-    $rand_index = rand(0, count($images));
-    $url = $images[$rand_index]['url'];
-    $pic_content = file_get_contents($url);
-    $name = substr($url, strrpos($url, '/') + 1);
-    Storage::put('public/images/' . $name, $pic_content);
-    $attachment = new Image($url);
-    $message = OutgoingMessage::create()
-        ->withAttachment($attachment);
-    $bot->reply('Your pic is here:');
-    $bot->reply($message);
+    $bot->reply('Your pics are here:');
+    $i = 0;
+    while ($i++ < 6) {
+        $images = GoogleImageGrabber::grab($keyword);
+        $rand_index = rand(0, count($images));
+        $url = $images[$rand_index]['url'];
+        $pic_content = file_get_contents($url);
+        $name = substr($url, strrpos($url, '/') + 1);
+        Storage::put('public/images/' . $name, $pic_content);
+        $attachment = new Image($url);
+        $message = OutgoingMessage::create()
+            ->withAttachment($attachment);
+        $bot->reply($message);
+    }
 });
