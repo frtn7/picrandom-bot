180db167ca9974aa1293a72f7d6acac3620ecb48
Dmitry Ivanoff <gausspoint34@gmail.com>
2021-11-03 22:49:09 +0300

    feat: random image command

--- routes/botman.php
+++ routes/botman.php
@@ -3,6 +3,7 @@ use App\Http\Controllers\BotManController;
 use Buchin\GoogleImageGrabber\GoogleImageGrabber;
 use Illuminate\Support\Facades\Storage;
 use BotMan\BotMan\Messages\Attachments\Image;
+use BotMan\BotMan\Messages\Outgoing\OutgoingMessage;
 
 $botman = resolve('botman');
 
@@ -14,6 +15,8 @@ $botman->hears('/pic {keyword}', function ($bot, $keyword) {
     $name = substr($url, strrpos($url, '/') + 1);
     Storage::put('public/images/' . $name, $pic_content);
     $attachment = new Image($url);
+    $message = OutgoingMessage::create()
+        ->withAttachment($attachment);
     $bot->reply('Your pic is here:');
-    $bot->reply($attachment);
+    $bot->reply($message);
 });
